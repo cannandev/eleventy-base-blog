921fc0a1ee6a6ab7b853c57dc3f6b249de788db6
Claire Annan <claire.annan@gsa.gov>
2021-05-03 23:01:58 -0400

    Add eleventy-plugin-time-to-read.

--- .eleventy.js
+++ .eleventy.js
@@ -5,12 +5,15 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const timeToRead = require('eleventy-plugin-time-to-read');
+
 
 module.exports = function(eleventyConfig) {
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(timeToRead);
 
   // https://www.11ty.dev/docs/data-deep-merge/
   eleventyConfig.setDataDeepMerge(true);
