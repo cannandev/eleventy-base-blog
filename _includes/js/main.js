62b7d6bd1e86a80caa0d71e26f386e963701ba63
Claire Annan <claire.annan@gsa.gov>
2021-05-21 00:25:33 -0400

    Show/hide desktop category dropdown.

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,2 +1,34 @@
-// Hi
-console.log("Hi");
+/* Articles Dropdown Menu */
+const mainNavToggle = document.getElementById("navDropdownToggle");
+const mainNavLinks = mainNavToggle.nextElementSibling;
+const mainNavCaret = mainNavToggle.querySelector("svg");
+let angle = 0;
+
+const isMainDropdownHidden = () => {
+  return mainNavLinks.classList.contains("hidden");
+}
+
+const rotateCaret = () => {
+  angle += 180;
+  if (angle == 360) {
+    rotation = 0;
+  }
+
+  mainNavCaret.style.transform = `rotate(${angle}deg)`;
+}
+
+const showDropdown = e => {
+  e.target.closest("button").setAttribute("aria-expanded", true);
+  rotateCaret();
+}
+
+const hideDropdown = e => {
+  e.target.closest("button").setAttribute("aria-expanded", false);
+  rotateCaret();
+}
+
+mainNavToggle.addEventListener('click', e => {
+  mainNavLinks.classList.toggle('hidden');
+
+  isMainDropdownHidden() ?  showDropdown(e) : hideDropdown(e);
+})
