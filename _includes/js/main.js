178bbace67847dcf5115c102f892837b9a9bb088
Claire Annan <claire.annan@gsa.gov>
2021-05-21 00:54:13 -0400

    Use StandardJS for linting. Fix missing caretRotation var.

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,34 +1,34 @@
 /* Articles Dropdown Menu */
-const mainNavToggle = document.getElementById("navDropdownToggle");
-const mainNavLinks = mainNavToggle.nextElementSibling;
-const mainNavCaret = mainNavToggle.querySelector("svg");
-let caretRotation = 0;
+const mainNavToggle = document.getElementById('navDropdownToggle')
+const mainNavLinks = mainNavToggle.nextElementSibling
+const mainNavCaret = mainNavToggle.querySelector('svg')
+let caretRotation = 0
 
 const isMainDropdownHidden = () => {
-  return mainNavLinks.classList.contains("hidden");
+  return mainNavLinks.classList.contains('hidden')
 }
 
 const rotateCaret = () => {
-  caretRotation += 180;
-  if (caretRotation == 360) {
-    rotation = 0;
+  caretRotation += 180
+  if (caretRotation === 360) {
+    caretRotation = 0
   }
 
-  mainNavCaret.style.transform = `rotate(${caretRotation}deg)`;
+  mainNavCaret.style.transform = `rotate(${caretRotation}deg)`
 }
 
 const showDropdown = e => {
-  e.target.closest("button").setAttribute("aria-expanded", true);
-  rotateCaret();
+  e.target.closest('button').setAttribute('aria-expanded', true)
+  rotateCaret()
 }
 
 const hideDropdown = e => {
-  e.target.closest("button").setAttribute("aria-expanded", false);
-  rotateCaret();
+  e.target.closest('button').setAttribute('aria-expanded', false)
+  rotateCaret()
 }
 
 mainNavToggle.addEventListener('click', e => {
-  mainNavLinks.classList.toggle('hidden');
+  mainNavLinks.classList.toggle('hidden')
 
-  isMainDropdownHidden() ?  showDropdown(e) : hideDropdown(e);
+  isMainDropdownHidden() ? showDropdown(e) : hideDropdown(e)
 })
