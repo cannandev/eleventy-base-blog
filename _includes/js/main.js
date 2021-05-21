41e6f20b09375e124c17f49cfa5852ed694c077e
Claire Annan <claire.annan@gsa.gov>
2021-05-21 00:26:28 -0400

    Update caret var.

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -2,19 +2,19 @@
 const mainNavToggle = document.getElementById("navDropdownToggle");
 const mainNavLinks = mainNavToggle.nextElementSibling;
 const mainNavCaret = mainNavToggle.querySelector("svg");
-let angle = 0;
+let caretRotation = 0;
 
 const isMainDropdownHidden = () => {
   return mainNavLinks.classList.contains("hidden");
 }
 
 const rotateCaret = () => {
-  angle += 180;
-  if (angle == 360) {
+  caretRotation += 180;
+  if (caretRotation == 360) {
     rotation = 0;
   }
 
-  mainNavCaret.style.transform = `rotate(${angle}deg)`;
+  mainNavCaret.style.transform = `rotate(${caretRotation}deg)`;
 }
 
 const showDropdown = e => {
