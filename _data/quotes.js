19f93e1b68144b86bbc99359c1c52892fccb16c5
Claire Annan <claire.annan@gsa.gov>
2022-01-07 15:57:11 -0500

    Update quotes collection from mongodb. Change properties in template accordingly.

--- _data/quotes.js
+++ _data/quotes.js
@@ -1,20 +1,11 @@
 var axios = require('axios');
 
-// module.exports = function () {
-//   return {
-//     collection: process.env.COLLECTION_NAME,
-//     db: process.env.DATABASE_NAME,
-//     key: process.env.API_KEY,
-//     source: process.env.DATA_SOURCE
-//   };
-// };
-
 module.exports = function () {
   let data = JSON.stringify({
     "collection": process.env.COLLECTION_NAME,
     "database": process.env.DATABASE_NAME,
     "dataSource": process.env.DATA_SOURCE,
-    "limit": 7
+    "limit": 10
   });
 
   let config = {
