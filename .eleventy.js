94cc3382755129b0eeec3365f482283a365f735b
Claire Annan <claire.annan@gsa.gov>
2022-01-07 00:41:35 -0500

    Add env variables with dotenv.

--- .eleventy.js
+++ .eleventy.js
@@ -7,9 +7,9 @@ const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const timeToRead = require("eleventy-plugin-time-to-read");
 const htmlmin = require('html-minifier');
+const config = require('dotenv').config();
 
-
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
@@ -27,21 +27,22 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
 
   eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd LLL yyyy");
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
   });
 
   eleventyConfig.addFilter('postDate', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('LLL dd, yyyy');
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('LLL dd, yyyy');
   });
 
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
+    if (n < 0)
+    {
       return array.slice(n);
     }
 
@@ -59,7 +60,7 @@ module.exports = function(eleventyConfig) {
   })
 
   // Create an array of all tags
-  eleventyConfig.addCollection("tagList", function(collection) {
+  eleventyConfig.addCollection("tagList", function (collection) {
     let tagSet = new Set();
     collection.getAll().forEach(item => {
       (item.data.tags || []).forEach(tag => tagSet.add(tag));
@@ -80,7 +81,8 @@ module.exports = function(eleventyConfig) {
       process.env.ELEVENTY_PRODUCTION &&
       outputPath &&
       outputPath.endsWith('.html')
-    ) {
+    )
+    {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
@@ -110,12 +112,12 @@ module.exports = function(eleventyConfig) {
   // Override Browsersync defaults (used only with --serve)
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, browserSync) {
+      ready: function (err, browserSync) {
         const content_404 = fs.readFileSync('_site/404.html');
 
         browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
-          res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
           res.write(content_404);
           res.end();
         });
@@ -130,10 +132,12 @@ module.exports = function(eleventyConfig) {
     code,
     callback
   ) {
-    try {
+    try
+    {
       const minified = await minify(code);
       callback(null, minified.code);
-    } catch (err) {
+    } catch (err)
+    {
       console.error("Terser error: ", err);
       // Fail gracefully.
       callback(null, code);
