ac5dcd104939b7b004753495d9001acfe34a2612
Claire Annan <claire.annan@gsa.gov>
2022-01-13 13:57:25 -0500

    Remove js/main.js. Add new add-quote.js partial.

---
+++ src/_add-quote.js
@@ -0,0 +1,10 @@
+// Add a quote to the Mongodb collection
+const form = document.getElementsById('form__quote-add')
+
+// Get all field values into an object
+
+// Validate each field
+
+// Submit object via axios
+
+// Submit object via quote.js
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import "./_mobileToggle.js"
 import "./_accordion.js"
+import "./_add-quote.js"
 
