56f0c670732f2161214aeedffcfe114e593acd52
Claire Annan <claire.annan@gsa.gov>
2022-01-13 19:40:03 -0500

    Make quote text and author required fields. Remove unnecessary listener on submit button.

--- src/_add-quote.js
+++ src/_add-quote.js
@@ -4,29 +4,27 @@ import DOMPurify from 'dompurify'
 const quoteForm = document.getElementById('form__quote-add')
 const quoteSubmit = quoteForm.getElementsByTagName('button')[1]
 
-quoteSubmit.addEventListener('click', e => {
-  e.preventDefault()
-  validateForm(quoteForm.elements)
-})
-
 const validateForm = form => {
   const quoteObject = {}
+  // TODO: check if form.text && form.author are not empty required)
   // Use a for loop instead of forEach, since `form.elements` is an object, not an array.
   for (let i = 0; i < form.length; i++) {
     const element = form[i];
 
     // If the form element has a name property (which buttons do not) sanitize, trim and add to the object.
     if (element.name) {
+      // TODO: Check that at least quote text and author have values
       quoteObject[element.name] = DOMPurify.sanitize(element.value.trim())
     }
   }
   console.log('quoteObject', quoteObject)
-  return false
+  if (Object.keys(quoteObject).length === 0) return false
+  return true
 }
 // Show success message when form is submitted.
 quoteForm.addEventListener('submit', e => {
   e.preventDefault()
-  if (!validateForm()) return
+  if (!validateForm(quoteForm.elements)) return
   quoteForm.submit()
   // TODO: sendForm(quoteForm.action, { quoteDocument })
   quoteForm.nextElementSibling.textContent = `Your quote has been successfully submitted.`
